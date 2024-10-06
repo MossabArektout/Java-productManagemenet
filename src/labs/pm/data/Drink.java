459b6ac7a067f634df5809cc56c74ed1db78636a
mossab arektout <mossabarektout2000@gmail.com>
2024-10-06 11:43:29 +0100

    create Food and Drink classes

---
+++ src/labs/pm/data/Drink.java
@@ -0,0 +1,13 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Mossab Arektout
+ **/
+public class Drink extends Product{
+    public Drink(int id, String name, BigDecimal price, Rating rating) {
+        super(id, name, price, rating);
+    }
+
+}
---
+++ src/labs/pm/data/Food.java
@@ -0,0 +1,20 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author Mossab Arektout
+ **/
+public class Food extends Product{
+    private LocalDate bestBefore;
+
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
+
+    public Food(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        super(id, name, price, rating);
+        this.bestBefore = bestBefore;
+    }
+}
