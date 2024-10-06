bdd6bd17a315cc0056c14cfb68b80c02a76f58b0
mossab arektout <mossabarektout2000@gmail.com>
2024-10-06 11:16:05 +0100

    improve class design

--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -8,36 +8,60 @@ import static java.math.RoundingMode.HALF_UP;
  * @author Mossab Arektout
  **/
 public class Product {
-    private int id;
-    private String name;
-    private BigDecimal price;
+    private final int id;
+    private final String name;
+    private final BigDecimal price;
     public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private Rating rating;
+
+    public Product(int id, String name, BigDecimal price, Rating rating){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price){
+        this(id, name, price, Rating.NOT_RATED);
+    }
+
+    public Product(){
+        this(0, "no name", BigDecimal.ZERO);
+    }
 
     public int getId() {
         return id;
     }
 
-    public void setId(final int id) {
-        this.id = id;
-    }
+//    public void setId(final int id) {
+//        this.id = id;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+//    public void setName(final String name) {
+//        this.name = name;
+//    }
 
     public BigDecimal getPrice() {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
+//    public void setPrice(final BigDecimal price) {
+//        this.price = price;
+//    }
+
+    public Rating getRating() {
+        return rating;
     }
 
     public BigDecimal getDiscount(){
         return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Product applyRating(Rating newRating){
+        return new Product(id, name, price, newRating);
     }
 }
