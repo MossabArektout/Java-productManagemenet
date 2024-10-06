5682e7ffa6bf73255849fa2a731b95e09714566f
mossab arektout <mossabarektout2000@gmail.com>
2024-10-06 10:46:43 +0100

    setting up the env

---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,43 @@
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+import static java.math.RoundingMode.HALF_UP;
+
+/**
+ * @author Mossab Arektout
+ **/
+public class Product {
+    private int id;
+    private String name;
+    private BigDecimal price;
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(final BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getDiscount(){
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+}
